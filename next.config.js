a935aa7ff6d41491daf39ff374602dbad7393ad0
disco07 <koneenok@outlook.fr>
2022-05-04 17:13:13 +0200

    testing code with jest

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  presets: ["@babel/preset-env", "@babel/preset-react", '@babel/preset-typescript'],
 }
 
 module.exports = nextConfig
