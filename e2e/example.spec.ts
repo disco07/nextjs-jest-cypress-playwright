b2ac16bfe923a81bdc58136519466528f067e5b8
disco07 <koneenok@outlook.fr>
2022-05-05 12:41:13 +0200

    testing code with playwright

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,8 +1,18 @@
 import { test} from '@playwright/test'
 
-test('should navigate to the about page', async ({ page }) => {
+test('should calculate operation on page', async ({ page }) => {
     // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
     await page.goto('http://localhost:3000/')
+    // Click [placeholder="Enter a first number"]
+    await page.locator('[placeholder="Enter a first number"]').click();
+    // Click [placeholder="Enter a second number"]
+    await page.locator('[placeholder="Enter a second number"]').click();
+    // fill [placeholder="Enter a first number"]
+    await page.locator('[placeholder="Enter a first number"]').fill("3");
+    // fill [placeholder="Enter a second number"]
+    await page.locator('[placeholder="Enter a second number"]').fill("3");
+    // Click button:has-text("Multiplication")
+    await page.locator('button:has-text("Multiplication")').click();
 
     await page.pause()
 })
