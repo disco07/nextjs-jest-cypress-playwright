1d9e311c7b9e25b360d18010ff5a794fcaae37d2
disco07 <koneenok@outlook.fr>
2022-05-05 11:20:23 +0200

    testing with cypress

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,11 +1,13 @@
+/// <reference types="cypress" />
+
 describe('Navigation ', () => {
     it('should calculate operation on page', () => {
         // Start from the index page
         cy.visit('http://localhost:3000/')
 
-        cy.get('input[name=number1]').type(9)
+        cy.get('input[name=number1]').type("9")
 
-        cy.get('input[name=number2]').type(3)
+        cy.get('input[name=number2]').type("3")
 
         cy.get('.addition').click()
         cy.get('.multiplication').click()
